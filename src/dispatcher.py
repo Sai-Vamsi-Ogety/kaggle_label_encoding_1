39cd8f1c598a51e12fffee6cec9fde42935c4730
Sai-Vamsi-Ogety <ogetyvamsi@gmail.com>
2020-01-16 13:05:36 -0700

    added one-hot-encoding

--- src/dispatcher.py
+++ src/dispatcher.py
@@ -1,6 +1,6 @@
 from sklearn import ensemble
 0.75091
 MODELS = {
-    "randomforest": ensemble.RandomForestClassifier(n_estimators=200, verbose=2),
+    "randomforest": ensemble.RandomForestClassifier(n_estimators=100, verbose=2),
     "extratrees": ensemble.ExtraTreesClassifier(n_estimators=200, verbose=2)
 }
